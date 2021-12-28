ca355d8cb7a970cb4ad170f62f14e4041c616470
adminer001 <96757256+adminer001@users.noreply.github.com>
2021-12-28 18:47:02 +0800

    Add files via upload

---
+++ AnalysisChart.IDal/IAnalyse_QuantityDataWithQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace AnalysisChart.IDal
+{
+    public interface IAnalyse_QuantityDataWithQuery
+    {
+        DataTable GetBlankTable(string myDataBaseName, string myDataTableName);
+        DataTable GetTableRowsValue(string myDataBaseName, string myDataTableName, string myDateColumnName, string[] myColumnName, string myStartTime, string myEndTime, string myAnalyiseType, string myStatisticsType);
+    }
+}
